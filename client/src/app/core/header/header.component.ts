10c2c8ac00bb537da7e24692183c4f5b254f1175
CraXWaR <craxwar@gmail.com>
2023-05-09 12:47:46 +0300

    Updating header

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  @ViewChild('dropdown') dropdown: any;
-
-  toggleDropdown(dropdown: any): void {
-    dropdown.nativeElement.classList.toggle('show');
-  }
-}
+export class HeaderComponent { }
