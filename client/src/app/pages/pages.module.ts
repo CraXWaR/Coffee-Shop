af0e6a355b2384f54725b33a0d834c0cb72216ec
CraXWaR <craxwar@gmail.com>
2023-05-10 11:44:17 +0300

    Creating details component

--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateComponent } from './create/create.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { DetailsComponent } from './details/details.component';
 
 
 
 @NgModule({
   declarations: [
     CreateComponent,
-    CatalogComponent
+    CatalogComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule
