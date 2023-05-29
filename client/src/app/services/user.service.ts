50b164ffcbe9ca390cd46fd5e68660a65e3c9016
CraXWaR <craxwar@gmail.com>
2023-05-29 11:59:10 +0300

    Creating register service funcion

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+const API_URL = 'http://localhost:3030';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: any | undefined ;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(data: {}) {
+    return this.http.post(`${API_URL}/register`, data).pipe(
+      tap((user) => {
+        this.user = user
+      })
+    )
+  }
 }
