d3a3a180809c3aff748f4e71f919c5550c976cec
CraXWaR <craxwar@gmail.com>
2023-05-29 12:05:42 +0300

    Creating the user interface props

--- client/src/app/shared/interfaces/user-interface.ts
+++ client/src/app/shared/interfaces/user-interface.ts
@@ -1,2 +1,9 @@
 export interface UserInterface {
+    username: string,
+    email: string,
+    accessToken: string,
+    _id: any,
+    cafes: string[],
+    favouriteCafes: string[],
+    avatarImg: string
 }
