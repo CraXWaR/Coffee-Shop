43be249b6a5a1a226a226ca2ae843ea3e5a0a13d
CraXWaR <craxwar@gmail.com>
2023-05-29 12:10:42 +0300

    Fixing register user service funcion

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+import { UserInterface } from '../shared/interfaces/user-interface';
 
 const API_URL = 'http://localhost:3030';
 
@@ -8,12 +9,12 @@ const API_URL = 'http://localhost:3030';
   providedIn: 'root'
 })
 export class UserService {
-  user: any | undefined ;
+  user: null | UserInterface | undefined ;
 
   constructor(private http: HttpClient) { }
 
   register(data: {}) {
-    return this.http.post(`${API_URL}/register`, data).pipe(
+    return this.http.post<UserInterface>(`${API_URL}/register`, data).pipe(
       tap((user) => {
         this.user = user
       })
