65db8f729b4edda1bb80bb4754d6992c12456e41
CraXWaR <craxwar@gmail.com>
2023-06-05 13:27:10 +0300

    Creating the coffee interface

--- client/src/app/shared/interfaces/coffee-interface.ts
+++ client/src/app/shared/interfaces/coffee-interface.ts
@@ -1,2 +1,13 @@
+import { UserInterface } from "./user-interface";
+
 export interface CoffeeInterface {
+    make: string,
+    type: string,
+    intensity: number,
+    imageUrl: string,
+    description: string,
+    price: number,
+    _id: string,
+    owner: UserInterface,
+    addedBy: UserInterface[],
 }
