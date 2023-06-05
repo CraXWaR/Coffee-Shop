6a2defa2a9503b807d8b44980f1c5e8c56da940c
CraXWaR <craxwar@gmail.com>
2023-06-05 13:41:38 +0300

    Creating add funcion in create component

--- client/src/app/pages/create/create.component.ts
+++ client/src/app/pages/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CoffeeService } from 'src/app/services/coffee.service';
 
 @Component({
   selector: 'app-create',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class CreateComponent {
 
+  errors: string | undefined = undefined;
+
+  constructor(private coffeeService: CoffeeService, private router: Router) { }
+
+  async addCoffee(form: NgForm){
+    this.coffeeService.addCoffee(form.value).subscribe({
+      next: () => {
+        this.router.navigate(['/catalog'])
+      },
+      error: (err) => {
+       console.log(err);
+       
+      }
+    })
+  }
 }
