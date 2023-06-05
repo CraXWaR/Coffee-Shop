9d6d60ffdb4399a6a59286b30c12ab76ffb20dc9
CraXWaR <craxwar@gmail.com>
2023-06-05 13:22:12 +0300

    Fixing server model for cafes

--- server/models/Coffee.js
+++ server/models/Coffee.js
@@ -6,43 +6,30 @@ const coffeeSchema = new mongoose.Schema({
         type: String,
         minlength: [3, 'You should have at least 3 characters!']
     },
-    model: {
+    type: {
         required: true,
         type: String,
         minlength: [2, 'You should have at least 2 characters!']
     },
-    hp: {
+    intensity: {
         required: true,
         type: Number,
-    },
-    carImages: {
-        required: true,
-        type: Array,
     },
     imageUrl: {
         required: false,
         type: String,
     },
-    imageId: {
-        type: String,
-    },
-    year: {
-        required: true,
-        type: String,
-        min: [1950, 'Car year should be newer than 1950\'s!'],
-        max: [2022, 'Car year cannot be in the future!']
-    },
     description: {
         required: true,
         type: String,
         minlength: [10, 'Description should have at least 10 characters!'],
-        maxlength: [50, 'Description shouldn\'t have more than 50 characters!'],
+        maxlength: [500, 'Description shouldn\'t have more than 500 characters!'],
 
     },
     price: {
         required: true,
         type: Number,
-        min: [500, 'Car should be more expensive than 500$!'],
+        min: [1, 'Coffee should be more expensive than 1$!'],
     },
     owner: {
         type: mongoose.Types.ObjectId,
