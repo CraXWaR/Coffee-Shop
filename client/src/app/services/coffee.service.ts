63abaf16666958f21f39d708bd58c27ff745d707
CraXWaR <craxwar@gmail.com>
2023-06-05 13:29:17 +0300

    Creating service to add cafes

--- client/src/app/services/coffee.service.ts
+++ client/src/app/services/coffee.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API_URL = 'http://localhost:3030';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoffeeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addCoffee(data: any) {
+    return this.http.post(`${API_URL}/cafes`, {data});
+  }
 }
