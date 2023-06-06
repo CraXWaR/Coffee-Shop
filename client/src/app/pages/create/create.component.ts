3d3723e5420803f6eabe308466dcb987d33d6a5a
CraXWaR <craxwar@gmail.com>
2023-06-06 13:23:33 +0300

    formating...

--- client/src/app/pages/create/create.component.ts
+++ client/src/app/pages/create/create.component.ts
@@ -19,7 +19,7 @@ export class CreateComponent {
     let token = localStorage.getItem('token');
     let value = form.value;
     value.token = token;
-    
+
     this.coffeeService.addCoffee(form.value).subscribe({
       next: () => {
         this.router.navigate(['/catalog']);
