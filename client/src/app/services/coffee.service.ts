c413c2b745bb36f8d482f4fa7471551c7f70b899
CraXWaR <craxwar@gmail.com>
2023-06-09 12:27:00 +0300

    creating request funcion for cafes

--- client/src/app/services/coffee.service.ts
+++ client/src/app/services/coffee.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CoffeeInterface } from '../shared/interfaces/coffee-interface';
 
 const API_URL = 'http://localhost:3030';
 
@@ -12,5 +13,9 @@ export class CoffeeService {
 
   addCoffee(data: any) {
     return this.http.post(`${API_URL}/cafes`, { data });
+  }
+
+  getAllCafes(){
+    return this.http.get<CoffeeInterface[]>(`${API_URL}/cafes`)
   }
 }
