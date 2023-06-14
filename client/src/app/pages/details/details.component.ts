01254c6053d9f11037dd807508f85a5098e82dc1
CraXWaR <craxwar@gmail.com>
2023-06-14 12:02:55 +0300

    Creating contructor and importing interfaces into details component

--- client/src/app/pages/details/details.component.ts
+++ client/src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CoffeeService } from 'src/app/services/coffee.service';
+import { CoffeeInterface } from 'src/app/shared/interfaces/coffee-interface';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
+  
+  coffee: CoffeeInterface | undefined;
+  errors: Object | undefined;
+
+  constructor(private coffeeService: CoffeeService, private activatedRoute: ActivatedRoute, private router: Router) {
+    
+  }
 
 }
