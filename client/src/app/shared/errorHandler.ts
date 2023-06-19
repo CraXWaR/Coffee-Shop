5d1bd42535eb6217b8cc5b31dc4274d98cf1eb8a
CraXWaR <craxwar@gmail.com>
2023-06-19 13:59:12 +0300

    Creating error handler and password and email validators

---
+++ client/src/app/shared/errorHandler.ts
@@ -0,0 +1,9 @@
+export function handleError(error: any) {
+    if (error.includes('ValidationError')) {
+        return error.split(': ')[2];
+    } else if (error.includes('Cast')) {
+        return error.split('CastError: ')[1];
+    } else {
+        return error;
+    }
+}
