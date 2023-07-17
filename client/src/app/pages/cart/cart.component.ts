7311b6ff76ae957df1aa15253f86fd96053d3298
CraXWaR <craxwar@gmail.com>
2023-07-17 11:42:31 +0300

    Creating cart

--- client/src/app/pages/cart/cart.component.ts
+++ client/src/app/pages/cart/cart.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+  constructor () {};
+
+  openModal() {
+    // Open the Bootstrap modal
+    // const modalElement = document.getElementById('buyModal');
+    // modalElement.classList.add('show');
+    // modalElement.style.display = 'block';
+    // document.body.classList.add('modal-open');
+  }
+
+  closeModal() {
+    // Close the Bootstrap modal
+    // const modalElement = document.getElementById('buyModal');
+    // modalElement.classList.remove('show');
+    // modalElement.style.display = 'none';
+    // document.body.classList.remove('modal-open');
+  }
+
+  buyProduct() {
+    // Perform the buy logic here
+    // ...
+
+    // Close the modal
+    this.closeModal();
+  }
+
+  removeProduct() {
+    // Perform the remove logic here
+    // ...
+  }
 }
